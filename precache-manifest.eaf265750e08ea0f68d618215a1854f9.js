8a5f3503a1dcb94cbd2490709e696ffa789991ad
whynot-vb <vlbunar@yahoo.com>
2019-08-31 18:18:50 +0200

    Updates

---
+++ precache-manifest.eaf265750e08ea0f68d618215a1854f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41bd35043221d2772dc5aded95a5d428",
+    "url": "/lovely-cats/index.html"
+  },
+  {
+    "revision": "183c294cf53d803e5217",
+    "url": "/lovely-cats/static/css/main.8442d099.chunk.css"
+  },
+  {
+    "revision": "3620637486f53dd09a08",
+    "url": "/lovely-cats/static/js/2.0eed3af6.chunk.js"
+  },
+  {
+    "revision": "183c294cf53d803e5217",
+    "url": "/lovely-cats/static/js/main.616b8cef.chunk.js"
+  },
+  {
+    "revision": "7f7691232e0eeb97fa94",
+    "url": "/lovely-cats/static/js/runtime~main.555331f1.js"
+  }
+]);
